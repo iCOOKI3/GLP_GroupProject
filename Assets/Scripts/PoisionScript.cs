e87bc154ba84582d42243150ead0132ed2565f94
ZeroTConcept <boppinecho@gmail.com>
2021-08-06 14:17:55 +0800

    update(not finalized)

---
+++ Assets/Scripts/PoisionScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PoisionScript : MonoBehaviour
+{
+    public int HP = 100;
+
+    public float totaltime = 0;
+
+    void OnTriggerStay(Collider other)
+    {
+        totaltime += Time.deltaTime;
+        if(totaltime>1)
+        {
+            HP -= 5;
+            Debug.Log("HP is " + HP + " total time is " + totaltime);
+                totaltime = 0;
+        }
+    }
+}
